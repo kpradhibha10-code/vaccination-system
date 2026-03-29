3c56540c81091ff6e2f39fef318e171dc8770638
kpradhibha10-code <kpradhibha10@gmail.com>
2026-03-29 22:00:54 +0530

    Add files via upload

---
+++ vaccination/src/main/java/com/example/vaccination/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.vaccination;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Vaccination System Running ✅";
+    }
+}
